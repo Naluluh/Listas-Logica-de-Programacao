03cc0a76e2549d2e609f06923ce3316d917edc60
Naluluh <nalu16luna@gmail.com>
2025-10-14 18:31:33 -0300

    Delete Python/Sala/Ex5L1.py

--- Python/Sala/Ex5L1.py
+++
@@ -1,5 +0,0 @@
-# Ler um número inteiro e exibir o resultado do quadrado dele mesmo.
-
-print("QUADRADO PERFEITO\n")
-n = int(input("Insira o número para consulta: "))
-print(f"O quadrado de {n} é {n*n}")
